6be3da8e7298acb006a2d52cb7c59cfa3725f809
Riley Mathews <rileymathews80@gmail.com>
2018-12-29 13:28:36 -0600

    added create player character action

--- app/controllers/player_characters_controller.rb
+++ app/controllers/player_characters_controller.rb
@@ -1,5 +1,22 @@
 class PlayerCharactersController < ApplicationController
-  def index; end
+  def index
+    @player_characters = PlayerCharacter.all
+  end
 
-  def new; end
+  def new
+    @player_character = PlayerCharacter.new
+  end
+
+  def create
+    @player_character = PlayerCharacter.new(player_character_params)
+    if @player_character.save
+      redirect_to player_characters_path
+    end
+  end
+
+  private
+
+  def player_character_params
+    params.require(:player_character).permit(:name, :level, :max_health)
+  end
 end
--- spec/features/player_character_spec.rb
+++ spec/features/player_character_spec.rb
@@ -4,12 +4,26 @@ RSpec.describe "player character pages" do
   it "can load the index page for characters" do
     visit player_characters_path
 
-    expect(page).to have_content "characters"
+    expect(page).to have_content "Characters"
   end
 
   it "can load the new page for characters" do
     visit new_player_character_path
 
     expect(page).to have_selector "form"
+    expect(page).to have_css "input#player_character_name"
+    expect(page).to have_css "input#player_character_level"
+    expect(page).to have_css "input#player_character_max_health"
+
+    fill_in "player_character_name", with: "Max Strongarm"
+    fill_in "player_character_level", with: "4"
+    fill_in "player_character_max_health", with: "45"
+    click_button "Create"
+
+    expect(page).to have_content "Max Strongarm"
+
+    visit player_characters_path
+
+    expect(page).to have_content "Max Strongarm"
   end
 end
