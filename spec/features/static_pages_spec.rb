2aaca9e9ffc13e634a96d24cf6e4942482f1efb6
Riley Mathews <rileymathews80@gmail.com>
2018-12-28 21:14:18 -0600

    got index page

---
+++ spec/features/static_pages_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe "loading the index page" do
+  it "can sucsessfuly load the index page" do
+    visit "/"
+
+    expect(page).to have_content "Welcome"
+  end
+end
